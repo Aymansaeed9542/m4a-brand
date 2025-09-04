5f60d3a908560bd90bbe48982292351bd749b142
Aymansaeed9542 <aym9542@gmail.com>
2025-09-04 16:29:05 +0300

    add navbar and routing setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,38 @@
-
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './App.css'
+import Navbar from './components/navbar/navbar.jsx'
+import Layout from './components/layout/layout.jsx'
+import Home from './components/home/home.jsx'
+import Products from './components/products/Products.jsx'
+import AboutUs from './components/aboutUs/AboutUs.jsx'
+import NotfoundPage from './components/notFoundPage/NotFoundPage.jsx'
+
 
 function App() {
-  return (
-    <>
+const router = createBrowserRouter([{
+  path :"" , element : <Layout/> , children :
+  [
+    {
+    index : true , element : <Home/>
+  } ,
+    {
+    path : "/" , element : <Home/>
+  },
+    {
+    path : "products" , element : <Products/>
+  },
+    {
+    path : "aboutus" , element : <AboutUs/>
+  },
+    {
+    path : "*" , element : <NotfoundPage/>
+  }
 
-    </>
+]
+}])
+
+  return (
+    <RouterProvider router={router}/>
   )
 }
 
---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,80 @@
+import { useState } from "react";
+import logo from "./../../assets/m4alogo.png"
+import { Link ,NavLink } from "react-router-dom";
+
+export default function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <div className="navbar bg-base-100 shadow-sm flex flex-column py-5 px-10">
+      <div className="flex-1">
+        <Link to="/">
+          <img
+            src={logo}
+            alt="M4A Logo"
+            className="w-15 sm:w-15 md:w-20 lg:w-22 xl:w-25 h-auto max-w-full"
+            style={{ objectFit: "contain" }}
+          />
+        </Link>
+      </div>
+      {/*toggle for small screens */}
+      <div className="flex-none lg:hidden">
+        <button
+          className="btn btn-ghost btn-circle"
+          onClick={() => setMenuOpen((open) => !open)}
+          aria-label="Toggle menu"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M4 6h16M4 12h16M4 18h16"
+            />
+          </svg>
+        </button>
+      </div>
+      {/* Main menu for small screens */}
+      <div
+        className={`${
+          menuOpen ? "block" : "hidden"
+        } absolute top-16 right-4 bg-base-100 rounded-box shadow-lg z-20 lg:hidden`}
+      >
+        <ul className="menu menu-vertical gap-2">
+          <li>
+            <NavLink to ="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/products">Products</NavLink>
+          </li>
+          <li>
+            <NavLink to="/aboutus">About Us</NavLink>
+          </li>
+        </ul>
+      </div>
+      {/* Main menu for large screens */}
+      <div className="hidden lg:flex gap-2 items-center">
+        <NavLink to="/" className="btn btn-ghost">
+          Home
+        </NavLink>
+        <NavLink to="/products" className="btn btn-ghost">
+          Products
+        </NavLink>
+        <NavLink to="/aboutus" className="btn btn-ghost">
+          About Us
+        </NavLink>
+        <input
+          type="text"
+          placeholder="Search"
+          className="input input-bordered w-24 md:w-auto"
+        />
+      </div>
+    </div>
+  );
+}
