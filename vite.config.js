c2b7e3eeff8793d7e74b026019a477d0ac0be97a
Aymansaeed9542 <aym9542@gmail.com>
2025-09-07 22:55:10 +0300

    solve diployment error

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,19 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react() , tailwindcss(),],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  preview: {
+    port: 3000,
+    strictPort: true,
+  },
+  server: {
+    port: 3000,
+    strictPort: true,
+  },
 })
