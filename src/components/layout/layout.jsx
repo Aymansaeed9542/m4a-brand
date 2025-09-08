bb7cfe89bc2e90d4a1769c00a6d49f5177348cdc
Aymansaeed9542 <aym9542@gmail.com>
2025-09-08 12:13:08 +0300

    Add useLocation + useEffect in layout comp ,smoothly scrolls to page top on every navigation and improves UX when moving between routes

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from './../../subComponent/navbar/navbar'
 import Footer from '../footer/footer'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 
 const Layout = () => {
+  const location = useLocation()
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })
+  }, [location.pathname])
+
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar/>
